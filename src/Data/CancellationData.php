a2d8a9054b011e1a685a237850b6cccdb45272a9
MilosTanaskovic <milos.tanaskovic@svenskadomaner.se>
2020-05-14 14:17:26 +0200

    test...

---
+++ src/Data/CancellationData.php
@@ -0,0 +1,27 @@
+<?php
+
+//jhkjhkjh
+/*
+* I won't put namespace and use here.
+* If you want to run this app you can put these here
+* In this file I'm gong to write just code ( without namespaces, uses and staf like that)
+*/
+
+/**
+ * Description of CancellationData
+ *
+ * @author MilosTanaskovic
+ */
+
+class CancellationData {
+ 
+    /**
+     * @var string
+     */
+    private $domainName;
+
+    /**
+     * @var string
+     */
+    private $hash;
+}
